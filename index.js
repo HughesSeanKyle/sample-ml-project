0d1de56c1031e41050508868cd3b6af22a92a482
HughesSeanKyle <khughessean@yahoo.com>
2023-04-02 12:24:47 +0200

    Join training and validation directories

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
 const path = require('path');
+
+const dataDir = './data';
+const trainDir = path.join(dataDir, 'train');
+const validationDir = path.join(dataDir, 'validation');
